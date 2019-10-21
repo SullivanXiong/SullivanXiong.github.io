13423970788f4eca5fba6f550d7f3391aeba3a70
Sullivan <sxiong5307@gmail.com>
2019-10-21 13:59:52 -0700

    fix

--- ng/src/app/models/session.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Session.Model } from './session.model';
-
-describe('Session.Model', () => {
-  it('should create an instance', () => {
-    expect(new Session.Model()).toBeTruthy();
-  });
-});
